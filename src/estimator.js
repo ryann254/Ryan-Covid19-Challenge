02f1859254bd09f35d3d6d357e5da5ac6b6e2823
ryann254 <keddelyronjoz@gmail.com>
2020-04-08 15:21:09 +0300

    removed data object

--- src/estimator.js
+++ src/estimator.js
@@ -1,17 +1,3 @@
-const data = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
-
 const calculator = (number, multiplyBy) => number * multiplyBy;
 
 const covid19ImpactEstimator = (data) => {
